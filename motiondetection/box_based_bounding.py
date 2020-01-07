dacf6dda9184031da05ff9a6f935a31f19f7cc00
Roshan Warman <roshanwarman22@gmail.com>
2020-01-06 21:32:49 -0500

    finished background initialization and edge cases for image block-segmentation

--- motiondetection/box_based_bounding.py
+++ motiondetection/box_based_bounding.py
@@ -23,42 +23,15 @@ import time
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
-
-#
-# class Block:
-#
-#     block_index = None
-#
-#     def __init__(self):
-#
-#     def set_block_index(self, horizontal, veritcal):
-#
-#         return $$
-#
-#     def adjacent_block_diff(self, block1, block2):
-#
-#         return difference
-#
 
 
+class Block:
 
 
-#
-#
-# def compute_SAD(block, image1, image2, image):
-#
-#     for t in width_array:
-#         frame = cv2.line(frame, (0, t), (frame_width, t), (255,0, 255), 1)
-#     for f in height_array:
-#         frame = cv2.line(frame, (f, 0), (f, frame_height), (255,0,255), 1)
-#
-#
-#     First_image = cv2.imread(image1)
-    # Second_image = cv2.imread(image2)
-#
-#
-#     np.abs(np.difference(First_image, Se
+    def __init__(self, value):
+        self.block_matrix = value
+        # self.index = index
+
 
 
 def create_blocks(image):
@@ -75,8 +48,14 @@ def create_blocks(image):
     blocks = []
     for i in range(len(width_array) -1 ):
         for j in range(len(height_array) - 1):
-            blocks.append(image[height_array[j] : height_array[j+1]], width_array[i] : width[array[i+1]])
-
+            if i == len(width_array) - 2 and j == len(height_array) - 2:
+                blocks.append(Block(image[height_array[j] : height -1, width_array[i] : width -1]))
+            elif i == len(width_array) - 1:
+                blocks.append(Block(image[height_array[j] : height_array[j+1], width_array[i] : width-1 ]))
+            elif j == len(height_array) -1:
+                blocks.append(Block(image[height_array[j] : height -1, width_array[i] : width_array[i+1]]))
+            else:
+                blocks.append(Block(image[height_array[j] : height_array[j+1], width_array[i] : width_array[i+1]]))
 
             #add edge blocks for image; this only goes to len(array) -1, so capture the end array separately
     return blocks
@@ -87,16 +66,19 @@ def SAD(image1, image2):
     im2_blocks = create_blocks(image_2)
     SAD = []
     for i in range(len(im1_block)):
-        SAD.append(np.sum(np.absolute(im1_block[i] - im2_block[i])))
+        SAD.append(np.sum(np.absolute(im1_block[i].value - im2_block[i].value)))
 
     return SAD
 
 
 
 def minimum_indexed_block(*images):
-    SAD_tensor = np.array([])
 
-    for i in range(len(images)-1):
+    SAD_tensor = SAD(images[0], images[1])
+
+    # blocks_tensor = create_blocks(images)
+
+    for i in range(1, len(images)-1):
         SAD_tensor = np.vstack(SAD_tensor, SAD(images[i], images[i+1]))
 
     flipped = SAD_tensor.transpose()
@@ -104,125 +86,110 @@ def minimum_indexed_block(*images):
 
     for i in flipped:
         minimum_blocks = np.concatenate(minmum_blocks, np.where(i == i.min())[0][0])
+    #minimum_blocks returns array [b1, b2, ... , bk] where b1 is the image that contains the arg min block in position (i,j) -> reshape
+    reconstructed_background = np.array([])
+    for i in range(len(minimum_blocks)):
+        reconstructed_background = np.concatenate(reconstructed_background, create_blocks[images[minimum_blocks[i]]][i])
 
-    return minimum_blocks
+    #reconstructed_background is 1D array of Block objects that are minimium SAD for each block location
+
+    background_initialization = reconstructed_background.reshape((image[0].shape))
+    background = np.zeros((background_initialization.shape))
+
+    #reshape and make background matrix which converts the matrix of Block objects to the values of the Block objects
+    for i in range(len(background)):
+        for j in range(len(background[i])):
+            background[i][j] = background_initialization[i][j].value
+
+
+    return background
+
 
 
 if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
 
-    image = cv2.imread("puc.JPG")
-    # print(image)
-    blocks = np.array(create_blocks(image))
-    first_block  = blocks[0]
-    second_block = blocks[1]
-    # plt.subplot(212)
-    # plt.imshow(first_block)
-    # plt.subplot(222)
-    # plt.imshow(second_block)
-    # plt.show()
-    print(SAD())
+    # first = None
+    i = 0
+    while(i  > 0):
 
-    cv2.waitKey(0)
+        for i in range()
+
+        image_sequence = np.array([])
+
+        for i in range(100):
+            t, frame = cap.read()
+            image_sequence = np.concatenate(image_sequence, frame)
+
+
+        cv2.imshow("current frame", frame)
+        cv2.imshow("helo", minimum_indexed_blocks(image_sequence))
+        if t:
+
+            frame = imutils.resize(frame, width=500)
+            gray1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            width, height = gray1.shape
+
+            width_array = np.array([x for x in range(0, width, round(width/16))])
+            height_array  = np.array([y for y in range(0, height, round(height/16))])
+
+
+            gridx, gridy= np.meshgrid(height_array, width_array)
+
+            # print(np.amax(height_array))
+            # print(np.amax(width_array))
+
+            frame_height, frame_width = gray1.shape
+            # print(gray1.shape)
+
+            # border for frame; consider removing later if SAD doesn't work
+
+            frame = cv2.line(frame, (0, 0), (0, frame_width), (0,0,0), 1)
+            frame = cv2.line(frame, (0, 0), (frame_height, 0), (0,0,0), 1)
+            frame = cv2.line(frame, (frame_height, frame_width), (0, frame_width), (0,255,0), 1)
+            frame = cv2.line(frame, (frame_height, 0), (frame_height, frame_width),  (0,255,0), 1)
+
+
+
+            for t in width_array:
+                frame = cv2.line(frame, (0, t), (frame_width, t), (255,0, 255), 1)
+            for f in height_array:
+                frame = cv2.line(frame, (f, 0), (f, frame_height), (255,0,255), 1)
+
+            gray = cv2.GaussianBlur(gray1, (21, 21), 0)
+
+            if first is None:
+                # first_time = time.time()
+                first = gray
+                continue
+
+            # if time.time() - first_time > 1:
+            #     first_time = time.time()
+            #     first = gray
+
+
+
+            frame_difference = cv2.absdiff(first, gray)
+            threshold = cv2.threshold(frame_difference, 25, 255, cv2.THRESH_BINARY)[1]
+
+            # threshold = cv2.dilate(threshold, None, iterations = 2)
+            countours = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+            countours = imutils.grab_contours(countours)
+
+            for c in countours:
+                if cv2.contourArea(c) < 10000:
+                    continue
+
+                (x, y, w, h) = cv2.boundingRect(c)
+                frame = cv2.rectangle(frame, (x,y), (x+w, y+h) , (0,255,0), 1)
+
+
+
+            cv2.imshow("constructionbox", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
     cv2.destroyAllWindows()
-# first = None
-# while(True):
-#
-#     t, frame = cap.read()
-#
-#     if t:
-#
-#         frame = imutils.resize(frame, width=500)
-#         gray1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#
-#         width, height = gray1.shape
-#
-#         width_array = np.array([x for x in range(0, width, round(width/16))])
-#         height_array  = np.array([y for y in range(0, height, round(height/16))])
-#
-#
-#         gridx, gridy= np.meshgrid(height_array, width_array)
-#
-#         # print(np.amax(height_array))
-#         # print(np.amax(width_array))
-#
-#         frame_height, frame_width = gray1.shape
-#         # print(gray1.shape)
-#
-#         # border for frame; consider removing later if SAD doesn't work
-#
-#         frame = cv2.line(frame, (0, 0), (0, frame_width), (0,0,0), 1)
-#         frame = cv2.line(frame, (0, 0), (frame_height, 0), (0,0,0), 1)
-#         frame = cv2.line(frame, (frame_height, frame_width), (0, frame_width), (0,255,0), 1)
-#         frame = cv2.line(frame, (frame_height, 0), (frame_height, frame_width),  (0,255,0), 1)
-#
-#
-#
-#         for t in width_array:
-#             frame = cv2.line(frame, (0, t), (frame_width, t), (255,0, 255), 1)
-#         for f in height_array:
-#             frame = cv2.line(frame, (f, 0), (f, frame_height), (255,0,255), 1)
-#
-#         gray = cv2.GaussianBlur(gray1, (21, 21), 0)
-#
-#         if first is None:
-#             # first_time = time.time()
-#             first = gray
-#             continue
-#
-#         # if time.time() - first_time > 1:
-#         #     first_time = time.time()
-#         #     first = gray
-#
-#
-#
-#         frame_difference = cv2.absdiff(first, gray)
-#         threshold = cv2.threshold(frame_difference, 25, 255, cv2.THRESH_BINARY)[1]
-#
-#         # threshold = cv2.dilate(threshold, None, iterations = 2)
-#         countours = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-#
-#         countours = imutils.grab_contours(countours)
-#
-#         for c in countours:
-#             if cv2.contourArea(c) < 10000:
-#                 continue
-#
-#             (x, y, w, h) = cv2.boundingRect(c)
-#             frame = cv2.rectangle(frame, (x,y), (x+w, y+h) , (0,255,0), 1)
-#
-#
-#
-#         cv2.imshow("constructionbox", frame)
-#
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
-#
-# # # import cv2
-# # # import numpy
-# # # import matplotlib.pyplot as plt
-# # #
-# # # cap = cv2.VideoCapture('movie.mp4')
-# # # t = True
-# # # while(t):
-# # #     t, frame = cap.read()
-# # #     if t:
-# # #
-# # #         cv2.imshow('video', frame)
-# # #
-# # #         if cv2.waitKey(1) & 0xFF  == ord('q'):
-# # #             break
-# # #
-# # # cap.release()
-# # # cv2.destroyAllWindows()
-# # # # img = cv2.imread('image.JPG', cv2.IMREAD_COLOR)
-# # # # cv2.imshow('image', img)
-# # # # cv2.waitKey(0)
-# # # # cv2.destroyAllWindows()
-# # #
-# # # # from urllib import request
-# # # #
-# # # # import re
-# # # #
-# # # # new_url = re.sub("tube", "pak", url_initial)
